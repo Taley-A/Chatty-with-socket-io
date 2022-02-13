9d3a1376029f5b215ce5e54e1625eda2c67214f0
Taley Ahmed <you@example.com>
2022-02-12 21:32:16 -0500

    Added dependencies required

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const path = require("path");
+const http = require("http");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 // const {
 
@@ -11,9 +14,27 @@ const morgan = require("morgan");
 const PORT = 8000;
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
+
+// Run when user connects
+io.on("connection", (socket) => {
+	console.log("New connection from " + socket.id);
+
+	socket.on("disconnect", () => {
+		console.log(`User ${socket.id} Disconnected.`);
+	});
+});
 
 app.get("/", (req, res) => {
 	res.status(200).json({
@@ -26,10 +47,10 @@ app.get("/", (req, res) => {
 app.get("*", (req, res) => {
 	res.status(404).json({
 		status: 404,
-		message: "This is obviously not what you are looking for.",
+		message: "This is obviously not what you're looking for.",
 	});
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.info("🌍 Listening on port " + PORT);
 });
